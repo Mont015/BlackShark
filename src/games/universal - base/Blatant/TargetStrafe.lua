fdb33d97a1ab0403d7db2d02f72a31a6884aebe2
Mont <diamantboshnjaku9@gmail.com>
2026-09-25 15:17:35 +0200

    Restore reliable TargetStrafe defaults

--- src/games/universal - base/Blatant/TargetStrafe.lua
+++ src/games/universal - base/Blatant/TargetStrafe.lua
@@ -157,11 +157,11 @@ Speed = TargetStrafe:CreateSlider({
 })
 TargetLock = TargetStrafe:CreateToggle({
 	Name = 'Target Lock',
-	Default = true,
+	Default = false,
 	Tooltip = 'Follows Killaura\'s current target when available.'
 })
 VoidSafety = TargetStrafe:CreateToggle({
 	Name = 'Void Safety',
-	Default = true,
+	Default = false,
 	Tooltip = 'Stops strafing when the next position has no ground below it.'
 })
