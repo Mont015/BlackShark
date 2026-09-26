a4eb1ab1657493bffe61afb06b0cf632c6eb6460
Mont <diamantboshnjaku9@gmail.com>
2026-09-26 11:50:11 +0200

    Restore fixed Killaura hit registration

--- src/games/bedwars/6872274481 - game/Blatant/Killaura.lua
+++ src/games/bedwars/6872274481 - game/Blatant/Killaura.lua
@@ -6,7 +6,6 @@ run(function()
 	local SwingRange
 	local AttackRange
 	local UpdateRate
-	local HitRate
 	local AngleSlider
 	local MaxTargets
 	local Mouse
@@ -30,6 +29,7 @@ run(function()
 	local AttackRemote
 	local LastManualSwing = 0
 	local NextAttack = 0
+	local HitInterval = 2 / 7
 	local AttackIndex = 1
 	local PrimaryTarget
 	local AnimationToken = 0
@@ -340,7 +340,7 @@ run(function()
 							local sent = attackTarget(sword, root, target)
 							if sent then
 								AttackIndex = (targetIndex % #attackable) + 1
-								NextAttack = now + (10 / HitRate.Value)
+								NextAttack = now + HitInterval
 							else
 								NextAttack = now + 0.1
 							end
@@ -431,13 +431,6 @@ run(function()
 		Max = 120,
 		Default = 60,
 		Suffix = 'hz'
-	})
-	HitRate = Killaura:CreateSlider({
-		Name = 'Hit rate',
-		Min = 1,
-		Max = 45,
-		Default = 35,
-		Suffix = 'hits / 10s'
 	})
 	MaxTargets = Killaura:CreateSlider({
 		Name = 'Max targets',
